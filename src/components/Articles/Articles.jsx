81155e4432434e5507e08c8eb2b1d793ee56d386
bayansuluu <bayanamanapbaeva@gmail.com>
2025-03-15 23:06:29 +0600

    add article

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import scss from "./Articles.module.scss";
+import { Link } from "react-router-dom";
+import ArticleSlider from "./ArticleSlider/ArticleSlider";
+import arrow from "../../assets/Article/arrow.svg";
+import { data } from "../../constants/Slider";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import "overlayscrollbars/styles/overlayscrollbars.css"; 
+import { VStack, Box, Text } from "@chakra-ui/react";
+
+function Articles() {
+  let recent = data.map(({ img, title, desc }, index) => (
+    <article key={index}>
+      <div className={scss.recentImg} ><img src={img} alt="" /></div>
+      <aside>
+        <p>{title}</p>
+        <span>{desc}</span>
+      </aside>
+      <Link to="/recentArticle" className={scss.btn}>
+        <img src={arrow} alt="arrow" />
+      </Link>
+    </article>
+  ));
+
+  return (
+    <div className={scss.article}>
+      <span className={scss.heading}>OUR ARTICLES</span>
+      <div className={scss.top}>
+        <h1>Stay Up-to-Date with Our VR Insights</h1>
+        <Link className={scss.btn} to="/recentArticles">
+          See all
+        </Link>
+      </div>
+     <main className={scss.mainBody}>
+     <ArticleSlider />
+      <div className={scss.sideBar}>
+        <h1>Recent Article</h1>
+        <main className={scss.recentMain}>{recent}</main>
+      </div>
+     </main>
+    </div>
+  );
+}
+
+export default Articles;
---
+++ src/constants/Slider.js
@@ -0,0 +1,39 @@
+import img1 from '../assets/Article/img1.svg'
+import img2 from '../assets/Article/img2.svg'
+
+
+export const media = ['Entertainment Goes Virtual: The Rise of VR Gaming', 'Entertainment Goes Virtual: The Rise of VR Gaming']
+
+export const data = [
+    {
+        img: img1,
+        title: "VR Education",
+        desc: "The Future of Education: How VR is Revolutionizing the Classroom"
+    },
+    {
+        img: img2,
+        title: "VR Architecture",
+        desc: "Bringing Designs to Life: How VR is Changing Architecture"
+    },
+    {
+        img: img1,
+        title: "VR Entertainment ",
+        desc: "Making Events Memorable: The Power of VR for Corporate and Special Occasions"
+    },
+    {
+        img: img2,
+        title: "VR Education",
+        desc: "The Future of Education: How VR is Revolutionizing the Classroom"
+    },
+    {
+        img: img1,
+        title: "VR Architecture",
+        desc: "Bringing Designs to Life: How VR is Changing Architecture"
+    },
+    {
+        img: img2,
+        title: "VR Entertainment ",
+        desc: "Making Events Memorable: The Power of VR for Corporate and Special Occasions"
+    },
+    
+]
